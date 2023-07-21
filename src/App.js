c501f7b72c79e1af70278601aa68f4f32d5b416d
Fabian Ruettgers <fabulon78@gmail.com>
2023-07-21 12:16:36 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter setPeople={setPeople} people={people} />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
